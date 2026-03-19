567e9efd01a197c58eb17e4c670159e3c5f23c87
Antoni Gdula <antoni.gdula08@gmail.com>
2026-03-19 08:56:07 +0100

    I make first full site on my microapplication

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list_of_question.html', views.list_of_question, name='list'),
 ]
--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'polls/index.html')
+def list_of_question(request):
+    return render(request, 'polls/list_of_question.html')
