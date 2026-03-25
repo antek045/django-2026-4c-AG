287ab4851f0bb4a44c63846ae0ea963ba22b4b7d
antek045 <antoni.gdula08@gmail.com>
2026-03-26 00:27:09 +0100

    Add login form and register form

--- myproject/mysite/urls.py
+++ myproject/mysite/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
 ]
 from django.contrib import admin
 from django.urls import include, path
+
 urlpatterns = [
 path("polls/", include("polls.urls")),
 path("admin/", admin.site.urls),
--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('list_of_question.html', views.list_of_question, name='list'),
+    path('login/', views.login_register_view, name='login'), 
 ]
